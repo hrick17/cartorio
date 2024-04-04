ab387c1560c303c1d749bbde134e19cc615e91de
hrick17 <166031234+hrick17@users.noreply.github.com>
2024-04-04 10:21:47 -0400

    opcao de saida
    incluso no sistema a opcao de saida pro usuario

--- ebac20.c
+++ ebac20.c
@@ -124,7 +124,9 @@ int main()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n\n"); 
+		printf("\t4 - sair do sistema\n\n");
 		printf("Opção: "); // Fim do menu
+		
 				
 		scanf("%d", &opcao); // Armazenando a escolha do usuário
 		
@@ -142,6 +144,11 @@ int main()
 		
 			    case 3:
 				deletar();
+				break;
+				
+				case 4:
+				printf("obrigado por utilizar o sistema!\n");
+				return 0;
 				break;
 		
 		     	default:
